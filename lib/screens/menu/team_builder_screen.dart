7d78642595723b3ffdace8f3ca471f922f19ca43
Thiago Rocha <thiago8rocha@gmail.com>
2026-03-23 21:35:18 -0300

    Fix TypeColors import in teams screens

--- lib/screens/menu/team_builder_screen.dart
+++ lib/screens/menu/team_builder_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:pokedex_tracker/theme/type_colors.dart';
 import 'package:pokedex_tracker/screens/detail/detail_shared.dart'
-    show ptType, typeIconAsset, TypeColors, calculateWeaknesses;
+    show ptType, typeIconAsset, calculateWeaknesses;
 import 'package:pokedex_tracker/services/dex_bundle_service.dart';
 import 'package:pokedex_tracker/services/pokeapi_service.dart';
 import 'package:pokedex_tracker/services/pokedex_data_service.dart';
